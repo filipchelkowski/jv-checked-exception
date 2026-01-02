ee5a7bd6ff2051851475049825db1e2ad841e005
filipchelkowski <filip.chelkowski@gmail.com>
2026-01-02 17:13:26 +0100

    Change line length in PasswordValidator

--- src/main/java/core/basesyntax/PasswordValidator.java
+++ src/main/java/core/basesyntax/PasswordValidator.java
@@ -5,12 +5,12 @@ public class PasswordValidator {
 
     }
 
-    public void validate(String password, String repeatPassword) throws PasswordValidationException {
-        if (password.equals(repeatPassword) && password.length() >= 10) {
+    public void validate(String pass, String repPass) throws PasswordValidationException {
+        if (pass.equals(repPass) && pass.length() >= 10) {
             return;
         }
 
-        if (password == null || repeatPassword == null) {
+        if (pass == null || repPass == null) {
             return;
         }
 
